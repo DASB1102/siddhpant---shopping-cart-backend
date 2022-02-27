86c8e9067a4f7b5c4eab02ea4e12c957068ab18c
DASB1102 <86590305+DASB1102@users.noreply.github.com>
2022-02-27 11:55:21 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,54 @@
+# Databases : 1-product 2-cart 3-invoice / Table :  product_details
+
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="postgres", user='postgres', password=12345, host='localhost', port= '5432'
+)
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Preparing query to create a database
+sql = '''CREATE database product;'''
+
+#Creating a database
+cursor.execute(sql)
+print("PRODUCT created successfully...")
+
+#Preparing query to create a database
+sql2 = '''CREATE database cart;'''
+
+#Creating a database
+cursor.execute(sql2)
+print("CART created successfully...")
+
+#Preparing query to create a database
+sql3 = '''CREATE database invoice;'''
+
+#Creating a database
+cursor.execute(sql3)
+print("INVOICE created successfully...")
+
+#Closing the connection
+conn.close()
+
+conn2 = psycopg2.connect(user="postgres", password="12345", database="product", host="localhost", port="5432")
+print("Successfully connected to product !")
+cursor2 = conn2.cursor()
+
+
+create_query="""CREATE TABLE product_details (BARCODE_NO BIGINT PRIMARY KEY ,
+                NAME VARCHAR(30) NOT NULL, 
+                PRICE_in_rs DECIMAL NOT NULL, 
+                WEIGHT_in_gms DECIMAL NOT NULL)"""
+cursor2.execute(create_query)
+print("product_details successfully created!")
+conn2.commit()
+
+conn2.close()
+
+
+
